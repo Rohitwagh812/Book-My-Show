fe414444c34f96c927d657dfce5af059a25f1509
Rohitwagh812 <rohitwagh480@gmail.com>
2023-10-16 23:09:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+// import { Route, Router } from 'react-router-dom';
+import { Button, Container, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logoimg from '../src/assets/Logo.png';
+import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Route, Routes,  useNavigate } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import SelectSeat from './pages/SelectSeat';
+import Success from './pages/Success';
+
+
+
+function App() {
+//  const navigate  = useNavigate()
+  const [user , setUser] = useState()
+  // const router =  createBrowserRouter([
+  //   {
+  //     path:'/login',
+  //     element:<Login setUser={setUser}/>
+  //   },
+  //   {
+  //     path:'/signUp',
+  //     element:<SignUp setUser={setUser}/>
+  //   },
+  //   {
+  //     path:'/',
+  //     element:<Home/>,
+  //   },
+  //   {
+  //     path:'/movie/:id',
+  //     element:<Movie/>,
+  //   },
+  //   {
+  //     path:'/seatpage',
+  //     element:<SelectSeat/>,
+  //   },
+  //   {
+  //     path:'/success',
+  //     element:<Success/>,
+  //   }
+  // ])
+
+  useEffect(() =>{
+    const userEmail = localStorage.getItem('userEmail')
+     if(userEmail){
+      setUser(userEmail)
+     }
+  },[user])
+
+  const handleLogout = () =>{
+    localStorage.removeItem('userEmail')
+    window.location.href = '/login'
+    // navigate('/login')
+    setUser(null)
+  }
+
+  return (
+    <div>
+       <Navbar bg='light' variant='light'>
+        <Container>
+          <Navbar.Brand style={{fontWeight:"bold"}} href="/">
+            <img
+              alt=""
+              src={Logoimg}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            TICKET BOX
+          </Navbar.Brand>
+          {user && <Button className='logout-btn' onClick={()=> {handleLogout()}}> Logout </Button>}
+        </Container>
+      </Navbar>
+      {/* <RouterProvider router={router}/> */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie/:id" element={<Movie/>}/>
+        <Route path="/seatpage" element={<SelectSeat/>}/>
+        <Route path="/success" element={<Success/>}/>
+        <Route path="/login" element={<Login setUser={setUser}/>}/>
+        <Route path="/signup" element={<SignUp setUser={setUser}/>}/>
+      </Routes>
+      </div>
+  )
+}
+
+export default App
+
+
